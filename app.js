3fa06da19b4b59b2cc617944b43a1bb616b31005
Pascal Rettig <pascal@cykod.com>
2012-08-28 16:40:08 -0400

    Initial Checkin

---
+++ app.js
@@ -0,0 +1,61 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , stylus = require('stylus')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('fdasfkhfio34gt73489g9fw7f9ew7g'));
+//  app.use(express.session());
+  app.use(app.router);
+  app.use(stylus.middleware({
+      src: __dirname + '/views' // .styl files are located in `views/stylesheets`
+    , dest: __dirname + '/public' // .styl resources are compiled `/stylesheets/*.css`
+    , compile: function(str, path) { // optional, but recommended
+		return stylus(str)
+		.set('filename', path)
+		.set('warn', true)
+		.set('compress', true);
+	}
+  }));
+  
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+// Socket.i
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+var io = require('socket.io').listen(server)
+
+
+io.sockets.on('connection',function(socket) {
+  
+  socket.on('pinger', function(data,fn) {
+    fn({ pong: new Date() });
+  });
+
+});
+
